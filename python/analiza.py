057f4a8f9a879552303e8ee3c7d30e23425d664c
4NGR0N <lecupra@gmail.com>
2025-11-26 16:25:48 +0100

    2025

--- python/analiza.py
+++ python/analiza.py
@@ -18,7 +18,7 @@ ipStart_cut=ipInicial_array[3]
 ipEnd_cut=ipFinal_array[3]
 
 #Definim una funció que recorre un set de ports PER A UNA IP, on el port indicat per l'usuari (portLimit) es l'últim.
-def scanPorts(ip):
+def scan_ports(ip):
     try:
         for acc in range(1, int(portLimit)):
             puerto = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -28,11 +28,11 @@ def scanPorts(ip):
                 print("PORT:"+str(acc)+" OPEN! on IP: "+str(ip))
             #else:
                 #print("PORT:"+str(acc)+" CLOSE? on IP: "+str(ip))
-            puerto.close
+            puerto.close()
     except:
         print("Usage: analiza <ip_start> <ip_end> <portLimit>")
 #Ara anem enviant a la funció abans definida varies IP's, compreses entre el limit introduït per l'usuari (IpInicial->IpFinal), alhora cadascun dels escanejos es farà en un 'fil' de la CPU diferent, enviant la execució de la funció principal (scanPorts) a un 'thread' cada cop.
 for acc2 in range(int(ipStart_cut),int(ipEnd_cut)):
     ipCompleta = ipInicial_array[0]+"."+ipInicial_array[1]+"."+ipInicial_array[2]+"."+str(acc2)
-    hilo = threading.Thread(target=scanPorts, args=(ipCompleta,))
+    hilo = threading.Thread(target=scan_ports, args=(ipCompleta,))
     hilo.start()
